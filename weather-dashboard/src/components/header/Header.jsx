092010d7489e6792ec94327893f9d06cf2737f54
Masum Billah <masumbillahyusuf1621@gmail.com>
2024-03-16 07:12:51 +0600

    ui structure complete and convert html template to react

---
+++ weather-dashboard/src/components/header/Header.jsx
@@ -0,0 +1,20 @@
+import FavoriteBar from "./FavoriteBar";
+import FavoriteListModal from "./FavoriteListModal";
+import Logo from "./Logo";
+import Search from "./Search";
+
+export default function Header() {
+  return (
+    <header className="fixed w-full right-0 top-0 z-50 bg-gradient-to-b from-black/60 to-black/0 pb-10">
+      <nav className="container flex items-center justify-between py-6">
+        <Logo />
+
+        <div className="flex items-center gap-4 relative">
+          <Search />
+          <FavoriteBar />
+          <FavoriteListModal />
+        </div>
+      </nav>
+    </header>
+  );
+}
