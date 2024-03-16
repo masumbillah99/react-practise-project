7f102eaf5e1db8394cc7984ac876f911ae726fdc
Masum Billah <masumbillahyusuf1621@gmail.com>
2024-03-16 19:47:08 +0600

    fetch data using separate hook and write a util function for formate date/time and Weather Headline show data complete

--- weather-dashboard/src/components/weather/WeatherHeadline.jsx
+++ weather-dashboard/src/components/weather/WeatherHeadline.jsx
@@ -1,22 +1,61 @@
-import cloud from "../../assets/cloud.svg";
+import { useContext } from "react";
+import { WeatherContext } from "../../context";
+import { getFormattedDate } from "../../utils/date-util";
+import cloudIcon from "../../assets/cloud.svg";
+import hazeIcon from "../../assets/haze.svg";
+import snowIcon from "../../assets/icons/snow.svg";
+import sunnyIcon from "../../assets/icons/sunny.svg";
+import rainIcon from "../../assets/rainy.svg";
+import thunderIcon from "../../assets/thunder.svg";
 import pinIcon from "../../assets/pin.svg";
 
 export default function WeatherHeadline() {
+  const { weatherData } = useContext(WeatherContext);
+
+  const { climate, location, temperature, time } = weatherData;
+
+  function getWeatherIcon(climate) {
+    switch (climate) {
+      case "Rain":
+        return rainIcon;
+      case "Clouds":
+        return cloudIcon;
+      case "Snow":
+        return snowIcon;
+      case "Clear":
+        return sunnyIcon;
+      case "Thunder":
+        return thunderIcon;
+      case "Fog":
+        return hazeIcon;
+      case "Haze":
+        return hazeIcon;
+      case "Mist":
+        return hazeIcon;
+
+      default:
+        return sunnyIcon;
+    }
+  }
+
   return (
     <div>
       <div className="max-md:flex items-center justify-between md:-mt-10">
-        <img src={cloud} alt="cloud" />
+        <img src={getWeatherIcon(climate)} alt="cloud" />
         <div className="max-md:flex items-center max-md:space-x-4">
           <h1 className="text-[60px] lg:text-[80px] xl:text-[100px] leading-none md:mb-4">
-            16°
+            {Math.round(temperature)}°
           </h1>
           <div className="flex items-center space-x-4 md:mb-4">
             <img src={pinIcon} />
-            <h2 className="text-2xl lg:text-[50px]">Dhaka</h2>
+            <h2 className="text-2xl lg:text-[50px]">{location}</h2>
           </div>
         </div>
       </div>
-      <p className="text-sm lg:text-lg">06:09 - Sunday, 9 Dec ‘23</p>
+      <p className="text-sm lg:text-lg">
+        {getFormattedDate(time, "time", false)} -{" "}
+        {getFormattedDate(time, "date", false)}
+      </p>
     </div>
   );
 }
---
+++ weather-dashboard/src/hooks/useWeather.js
@@ -0,0 +1,91 @@
+import { useEffect, useState } from "react";
+
+/** Data get korar 2 ta process:
+
+ * 1. আমি রেসপন্সটা নিয়ে নিলাম ও পুরো রেসপন্সটা আমাদের কম্পোনেন্টকে দিয়ে দিলাম,
+    এবং প্র্রত্যেক কম্পোনেন্টে গিয়ে রেসপন্সটা বেছে বেছে প্রয়োজনীয় জিনিসপত্র বা অপশনস নিয়ে
+    আমাদের কম্পোনেন্টে JSX এ দেখালাম।
+
+ * 2. আমরা হুকের মধ্যে ডাটা মডেলিং বা Data structure করতে পারি
+*/
+
+const useWeather = () => {
+  const [weatherData, setWeatherData] = useState({
+    location: "",
+    climate: "",
+    temperature: "",
+    maxTemperature: "",
+    minTemperature: "",
+    humidity: "",
+    cloudPercentage: "",
+    wind: "",
+    time: "",
+    latitude: "",
+    longitude: "",
+  });
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState({ state: false, message: "" });
+
+  // asynchronous calls for response
+  const fetchWeatherData = async (lat, lon) => {
+    try {
+      setLoading({
+        ...loading,
+        state: true,
+        message: "Fetching weather data...",
+      });
+
+      const response = await fetch(
+        `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${
+          import.meta.env.VITE_WEATHER_API_KEY
+        }&units`
+      );
+      // const response = await fetch(
+      //   `https://api.openweathermap.org/data/2.5/weather?${lat}=44.34&${lon}=10.99&appid=${
+      //     import.meta.env.VITE_WEATHER_API_KEY
+      //   }&units=metric`
+      // );
+      if (!response.ok) {
+        const errMsg = `Fetching weather data failed: ${response.status}`;
+        throw new Error(errMsg);
+      }
+
+      const data = await response.json();
+      const updateWeatherData = {
+        ...weatherData,
+        location: data?.name,
+        climate: data?.weather[0]?.main,
+        temperature: data?.main?.temp,
+        maxTemperature: data?.main?.temp_max,
+        minTemperature: data?.main?.temp_min,
+        humidity: data?.main?.humidity,
+        cloudPercentage: data?.clouds?.all,
+        wind: data?.wind?.speed,
+        time: data?.dt,
+        latitude: lat,
+        longitude: lon,
+      };
+      setWeatherData(updateWeatherData);
+    } catch (err) {
+      setError(err);
+    } finally {
+      setLoading({ ...loading, state: false, message: "" });
+    }
+  };
+
+  useEffect(() => {
+    setLoading({ state: true, message: "Finding location..." });
+
+    navigator.geolocation.getCurrentPosition((position) => {
+      fetchWeatherData(position.coords.latitude, position.coords.longitude);
+    });
+  }, []);
+
+  return {
+    weatherData,
+    error,
+    loading,
+  };
+};
+
+export default useWeather;
